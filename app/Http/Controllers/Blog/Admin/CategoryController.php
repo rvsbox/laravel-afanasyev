6bd784d9b0791835fffcf92f4fdb101e8a7a0fec
rvsbox <rvsbox@live.ru>
2019-10-12 16:02:56 +0200

    17 displayed all categories and pagination

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use App\Models\BlogCategory;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
-class CategoryController extends Controller
+class CategoryController extends BaseController
 {
     /**
      * Display a listing of the resource.
@@ -14,7 +14,12 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        dd(__METHOD__);
+        $dsd = BlogCategory::all();
+        $paginator = BlogCategory::paginate(5); // на каждую страницу по 5 элементов
+
+        dd($dsd, $paginator);
+
+        return view('blog.admin.category.index', compact('paginator'));
     }
 
     /**
